5a904a59e7cea5cae0614e61b1ae71d2bb5009f4
Bagdat <68787724+BagiYak@users.noreply.github.com>
2021-10-04 00:01:51 +0600

    tested Ktor client with server
    + Client - is https://github.com/BagiYak/ktor-client-test
    + Server - is https://github.com/BagiYak/ktor-api-server-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 val ktor_version: String by project
 val kotlin_version: String by project
+val kotlin_serialization: String by project
 val logback_version: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 group = "com.example"
@@ -18,10 +20,16 @@ repositories {
 }
 
 dependencies {
+
     // client
-    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
+
+    // serialization
+    implementation("io.ktor:ktor-serialization:$kotlin_serialization")
 
     // log
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
+
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,19 +3,24 @@ package com.example
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 suspend fun main() {
 
-    val client = HttpClient(CIO)
+    val client = HttpClient(CIO) {
+        install(Logging)
+    }
+
     val response: HttpResponse = client.get("https://ktor.io/")
+    println(response)
     println(response.status)
     println(response.responseTime)
     println(response.version)
     println("-------------")
 
-    val responseServer: HttpResponse = client.get("http://0.0.0.0:8080")
+    val responseServer: HttpResponse = client.get("http://127.0.0.1:8080/customer")
     println(responseServer)
     println(responseServer.status)
     println(responseServer.responseTime)
